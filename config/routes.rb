c4719d44f644c24775648c149e87ed4f3de4bc72
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-28 16:35:12 -0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
+  resources :users
 
   # get  '/help',    to: 'static_pages#help'
   # get  '/about',   to: 'static_pages#about'
