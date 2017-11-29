37322947534a55c74dba2a921a44d6ca65c4b173
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-11-29 14:00:18 -0600

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   # get  '/help',    to: 'static_pages#help'
